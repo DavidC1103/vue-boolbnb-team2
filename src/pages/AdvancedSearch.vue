ac41b6a1914df6da438205670a4ab177b72f49cc
raffaele-catalano <raf.catalano@gmail.com>
2023-07-24 20:16:14 +0200

    added content to page

--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -1,14 +1,16 @@
 <script>
 import { store } from '../data/store';
 import axios from 'axios';
+import Header from '../components/Header.vue';
 
 export default {
+  components: { Header },
   name: 'AdvancedSearch',
   data() {
     return {
       latitude: 40.803755,
       longitude: 14.198047,
-      radius: 50000,
+      radius: 30,
       arrSearch :[],
       store
     };
@@ -37,12 +39,54 @@ export default {
 
 </script>
 
+
 <template>
+
+  <Header/>
+
+  <h3>Risultati per: <span class="">{{ store.inputText  }}</span></h3>
+
+  <!-- Cards container -->
+  <div class="container-card d-flex flex-wrap justify-content-center">
+
+<!-- Single card -->
+    <div
+        class="boolbnb-card"
+        v-for="apartment in arrSearch"
+        :key="apartment.id">
+
+        <img :src=" 'http://127.0.0.1:8000/storage/' +  apartment.cover_image ">
+        <h3>{{ apartment.title }}</h3>
+        <p>{{ apartment.address }}</p>
+        <h6><strong>{{ apartment.price }} &euro;</strong> a notte</h6>
+
+        <span><router-link :to="{name:'detailApartment', params:{slug:apartment.slug}}">Dettaglio</router-link></span>
+        
+    </div>
+
+</div>
 
 </template>
 
 
-<style lang="scss" scoped>
+<style lang="scss" >
+.container-card{
+            width: 100%;
+            height: 100%;
+            .boolbnb-card{
+                flex-basis: 20%;
+                margin: 10px 20px;
+                img{
+                    width: 100%;
+                    height: 250px;
+                    object-fit: cover;
+                    border-radius: 10px;
+                }
 
+                p{
+                    color: gray;
+                }
+            }
+        }
 </style>
 
