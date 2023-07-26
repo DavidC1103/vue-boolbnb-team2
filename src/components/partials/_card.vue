fba9c22867bb3c917350300e8ea92f46922567a9
marcocnc <marcocancelliere56@gmail.com>
2023-07-26 11:43:44 +0200

    modified card file with props

--- src/components/partials/_card.vue
+++ src/components/partials/_card.vue
@@ -1,8 +1,15 @@
 <script>
-import { store } from '../data/store';
+import { store } from '../../data/store';
 
 export default {
+    name: 'card',
 
+    props:{
+        img : String,
+        title : String,
+        address: String,
+        price : Number
+    }
 }
 </script>
 
@@ -10,15 +17,16 @@ export default {
     <!-- Single card -->
     <div
         class="boolbnb-card"
-        v-for="apartment in store.arrApartments"
-        :key="apartment.id">
+        >
 
-        <img :src=" 'http://127.0.0.1:8000/storage/' +  apartment.cover_image ">
+        <!-- <img :src=" 'http://127.0.0.1:8000/storage/' +  apartment.cover_image ">
         <h3>{{ apartment.title }}</h3>
         <p>{{ apartment.address }}</p>
-        <h6><strong>{{ apartment.price }} &euro;</strong> a notte</h6>
-
-        <span><router-link :to="{name:'detailApartment', params:{slug:apartment.slug}}">Dettaglio</router-link></span>
+        <h6><strong>{{ apartment.price }} &euro;</strong> a notte</h6> -->
+        <img :src="img">
+        <h3>{{ title }}</h3>
+        <p>{{ address }}</p>
+        <h6><strong>{{ price }} &euro;</strong> a notte</h6>
 
     </div>
     
