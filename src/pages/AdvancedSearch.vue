a5a8402be40c1e690df9887d8de37ce8a1921ed3
mcspe <mcspezzaferro@gmail.com>
2023-07-28 01:45:56 +0200

    [add] partial responsiveness and api fix

--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -5,43 +5,80 @@ import Header from '../components/Header.vue';
 import CardItem from '../components/partials/CardItem.vue';
 
 export default {
-  components: { 
+  components: {
     Header,
     CardItem
   },
   name: 'AdvancedSearch',
   data() {
     return {
-      latitude: 40.803755,
-      longitude: 14.198047,
-      radius: 30,
-      arrSearch :[],
-      store
+      store,
+      arrSearch: [],
+      serviceOpen: false
     };
   },
   methods: {
+    getAllApartments() {
+      axios.get(store.apiUrl + 'apartments')
+        .then(result => {
+          store.arrApartments = result.data.total_apartments;
+          store.sponsoredApt = result.data.sponsored_apartments;
+          store.arrServices = result.data.availableServices;
+          this.sortApartments(store.arrApartments);
+        });
+    },
     searchApartments() {
       const requestData = {
         input: store.inputText,
         radius: store.rangeValue,
         services: store.servicesToSearch
       };
-
-      // console.log('requestData:', requestData.radius);
-
       axios.post(store.apiUrl + 'search', requestData)
         .then(response => {
-          this.arrSearch = response.data.apartments;
-          console.log(this.arrSearch); 
-      })
+          store.searchedApt = response.data.apartments;
+          this.sortApartments(store.searchedApt);
+        });
     },
-    refreshSearch(id){
-      if(store.servicesToSearch.includes(id)){
+    sortApartments(array) {
+      if (!Array.isArray(array)) {
+        this.arrSearch = [];
+        return;
+      }
+
+      const sponsoredSet = new Set(store.sponsoredApt.map(apartment => apartment.id));
+      const sortedApartments = array.sort((a, b) => {
+        const isAInSponsored = sponsoredSet.has(a.id);
+        const isBInSponsored = sponsoredSet.has(b.id);
+
+        if (isAInSponsored && isBInSponsored) {
+          // Both apartments are in sponsoredApt array, maintain their original order
+          return 0;
+        } else if (isAInSponsored) {
+          // Only apartment A is in sponsoredApt array, prioritize it
+          return -1;
+        } else if (isBInSponsored) {
+          // Only apartment B is in sponsoredApt array, prioritize it
+          return 1;
+        } else {
+          // Both apartments are not in sponsoredApt array, maintain their original order
+          return 0;
+        }
+      });
+
+      this.arrSearch = sortedApartments;
+    },
+    refreshSearch(id) {
+      if (store.servicesToSearch.includes(id)) {
         store.servicesToSearch.splice(store.servicesToSearch.indexOf(id), 1);
-      }else{
+      } else {
         store.servicesToSearch.push(id)
       }
       this.searchApartments();
+    },
+    resetSearch() {
+      if(!store.inputText) {
+        this.searchApartments();
+      }
     },
     inSequence(a, b, c) {
       let d = b - a;
@@ -65,24 +102,27 @@ export default {
         }
         return 0;
       }
+    },
+    toggleServices() {
+      this.serviceOpen = !this.serviceOpen;
     }
   },
   mounted() {
     store.servicesToSearch = [];
     store.rangeValue = 20;
-    this.searchApartments();
-
-    var rangeSlider = document.getElementById("rs-range-line");
-    var rangeBullet = document.getElementById("rs-bullet");
-
-    console.log(rangeSlider, rangeBullet)
-
+    this.getAllApartments();
+    // console.log(this.arrSearch);
+    if(store.inputText) {
+      this.searchApartments();
+    }
+    
+    const rangeSlider = document.getElementById("rs-range-line");
+    const rangeBullet = document.getElementById("rs-bullet");
+    
     rangeSlider.addEventListener("input", showSliderValue, false);
 
     function showSliderValue() {
       rangeBullet.innerHTML = rangeSlider.value;
-      // var bulletPosition = (rangeSlider.value /rangeSlider.max);
-      rangeBullet.style.left = (bulletPosition * 578) + "px";
     }
   }
 }
@@ -90,178 +130,246 @@ export default {
 
 
 <template>
+  <Header class="header-bg" />
 
-  <Header class="header-bg"/>
-
-  <div class="container advanced">
-    <h3>Risultati per: <span class="">{{ store.inputText  }}</span></h3>
-
-    <form action="" class="d-flex">
-
-      <div class="mb-5">
-          <h5 class="mb-3">FILTRI</h5>
-
-          <!-- INPUT RANGE -->
-          <div class="range-slider">
-            <span id="rs-bullet" class="rs-label">{{ store.rangeValue }}</span>
-            <input id="rs-range-line" class="rs-range" type="range" v-model="store.rangeValue"  min="0" max="200" step="10" @change="searchApartments()">
-            
-          </div>
-          
-          <div class="box-minmax">
-            <span>0</span><span>200</span>
-          </div>
-
-          <div class="d-grid gap-2">
-            <!-- Checkboxes -->
-            <div class="d-flex flex-wrap mt-4">
-            
-            <div class="form-check" v-for=" service in store.arrServices" :key="service.id">
-              <input class="form-check-input" type="checkbox" value="service.id" id="jobTitleCheckbox1" @click="refreshSearch(service.id)">
-              <label class="form-check-label d-flex" for="jobTitleCheckbox1">{{service.name}} <span class="ms-auto"></span></label>
-            </div>
-
+  <div class="container advanced py-5">
+    <h2 v-if="store.inputText">Ecco i risultati per la tua ricerca</h2>
+    <h2 v-else>Digita qualcosa per affinare la tua ricerca</h2>
+    <div class="d-flex flex-column my-4 p-3">
+      <h4 class="mb-3">FILTRI</h4>
+      <div class="d-flex align-items-md-center flex-column flex-md-row justify-content-between mb-5">
+        <!-- TEXT INPUT -->
+        <div class="search-text position-relative mb-3 me-md-5">
+          <input class="input-text p-3 w-100"
+            type="text"
+            v-model="store.inputText"
+            placeholder="Cerca per città"
+            @keyup.enter="searchApartments()"
+            @keyup.delete="resetSearch()">
+          <i class="fa-solid fa-magnifying-glass position-absolute search-btn" @click="searchApartments()"></i>
+        </div>
+        <!-- INPUT RANGE -->
+        <div v-if="store.inputText" class="range-slider d-flex align-items-center py-3">
+          <input id="rs-range-line"
+            class="rs-range"
+            type="range"
+            v-model="store.rangeValue"
+            min="0"
+            max="200"
+            step="10"
+            @change="searchApartments()">
+          <span id="rs-bullet"
+            class="rs-label">{{ store.rangeValue }}</span>
+        </div>
+      </div>
+      <div class="services">
+        <div class="services-title d-flex justify-content-center gap-2" :class="[serviceOpen ? 'align-items-center' : 'align-items-start']" @click="toggleServices()">
+          <h5>SERVIZI</h5>
+          <i v-if="serviceOpen" class="fa-solid fa-sort-up"></i>
+          <i v-else class="fa-solid fa-sort-down"></i>
+        </div>
+        <div class="services-display row mt-3 p-5 justify-content-center align-items-center text-center" :hidden="!serviceOpen">
+          <div class="form-check col-9 col-md-5 col-lg-3 col-xl-2"
+            v-for="service in store.arrServices"
+            :key="service.id">
+              <input class="form-check-input"
+                type="checkbox"
+                hidden
+                value="service.id"
+                :id="'service' + service.id"
+                @click="refreshSearch(service.id)">
+              <label class="form-check-label"
+              :for="'service' + service.id">{{ service.name }}</label>
           </div>
         </div>
-                  
       </div>
-    </form>
-
+    </div>
     <!-- Cards container -->
     <div class="container-card d-flex flex-wrap justify-content-center pb-5">
 
-    
-      <CardItem
-        class="boolbnb-card active reveal"
+
+      <CardItem class="boolbnb-card active reveal"
         v-for="(apartment, index) in arrSearch"
         :key="index"
-        :class="{'fade-left': inSequence(1, index + 1, 3), 'fade-top': inSequence(2, index + 1, 3), 'fade-right': inSequence(3, index + 1, 3)}"
+        :class="{ 'fade-left': inSequence(1, index + 1, 3), 'fade-top': inSequence(2, index + 1, 3), 'fade-right': inSequence(3, index + 1, 3) }"
         :img="'http://127.0.0.1:8000/storage/' + apartment.cover_image"
         :title="apartment.title"
         :address="apartment.address"
         :price="apartment.price"
-        @click="$router.push('/detail-apartment/' + apartment.slug)"
-      />
-      
+        @click="$router.push('/detail-apartment/' + apartment.slug)" />
+
     </div>
   </div>
 </template>
 
-
 <style lang="scss" scoped>
-.advanced{
-  h3{
-    margin-top: 80px;
+@use '../scss/partials/vars' as *;
+
+.advanced {
+  h2 {
+    margin-top: 60px;
   }
-  .form-check{
-    margin-right: 40px;
-    .ms-auto{
-      margin-left: 5px !important;
+  .search-text {
+    width: 100%;
+    .input-text {
+      border: 1px solid $federal_blue;
+      border-radius: 2rem;
+    }
+
+    .search-btn {
+      cursor: pointer;
+      font-size: 2.5rem;
+      color: $federal_blue;
+      top: .5rem;
+      right: 1rem;
+    }
+  }
+  .rs-label {
+    position: relative;
+    transform-origin: center center;
+    display: block;
+    width: 50px;
+    height: 50px;
+    background: transparent;
+    border-radius: 50%;
+    line-height: 50px;
+    text-align: center;
+    font-weight: bold;
+    padding-top: 5px;
+    box-sizing: border-box;
+    border: 2px solid $federal_blue;
+    // margin-top: 10px;
+    left: attr(value);
+    color: $federal_blue;
+    font-style: normal;
+    font-weight: normal;
+    line-height: normal;
+    font-size: 20px;
+
+    &::after {
+      content: "KM";
+      display: block;
+      font-size: 15px;
+      letter-spacing: 0.07em;
+      margin-top: -2px;
+    }
+  }
+  // .box-minmax {
+  //   margin-top: 10px;
+  //   width: 100%;
+  //   display: flex;
+  //   justify-content: space-between;
+  //   align-items: center;
+  //   font-size: 20px;
+  //   color: $federal_blue;
+  // }
+  .rs-range {
+    margin: 0 2rem;
+    width: 80%;
+    appearance: none;
+    -webkit-appearance: none;
+
+    &:focus {
+      outline: none;
+    }
+
+    &::-webkit-slider-runnable-track {
+      width: 100%;
+      height: 1px;
+      cursor: pointer;
+      box-shadow: none;
+      background: $federal_blue;
+      border-radius: 0px;
+      border: 0px solid #010101;
+    }
+
+    &::-moz-range-track {
+      width: 100%;
+      height: 1px;
+      cursor: pointer;
+      box-shadow: none;
+      background: $federal_blue;
+      border-radius: 0px;
+      border: 0px solid #010101;
+    }
+
+    &::-webkit-slider-thumb {
+      box-shadow: none;
+      border: 0px solid #ffffff;
+      box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.25);
+      height: 42px;
+      width: 22px;
+      border-radius: 22px;
+      background: $federal_blue;
+      cursor: pointer;
+      -webkit-appearance: none;
+      margin-top: -20px;
+    }
+
+    &::-moz-range-thumb {
+      box-shadow: none;
+      border: 0px solid $federal_blue;
+      box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.25);
+      height: 42px;
+      width: 22px;
+      border-radius: 22px;
+      background: $federal_blue;
+      cursor: pointer;
+      appearance: none;
+      -webkit-appearance: none;
+      margin-top: -20px;
+    }
+
+    &::-moz-focus-outer {
+      border: 0;
+    }
+  }
+  .services {
+    user-select: none;
+  }
+  .services-display {
+    background: #f7f7f7;
+  }
+  .form-check {
+    padding: 0;
+    margin: .5rem;
+    label {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 4rem;
+      outline: 1px solid $federal_blue;
+      outline-offset: -1px;
+      padding: .5rem 2rem;
+      border-radius: 2rem;
+      color: $federal_blue;
+      font-weight: bold;
+      transition: all .5s linear;
+    }
+    input:checked + label {
+      background: $argentinian_blue;
+      color: white;
+      outline: none;
+      transition: all .5s linear;
     }
   }
 }
-.container-card{
+
+.container-card {
   width: 100%;
   height: 100%;
 }
 
-
-.box-minmax{
-  margin-top: 10px;
-  width: 608px;
-  display: flex;
-  justify-content: space-between;
-  font-size: 20px;
-  color: black;
-  span:first-child{
-    margin-left: 10px;
+@media (min-width: 768px){
+  .advanced {
+    .search-text {
+      width: 100%;
+    }
+    .range-slider {
+      width: 60% !important;
+    }
   }
-}
-
-.rs-range {
-  margin-top: 29px;
-  width: 600px;
-  appearance: none;
-  -webkit-appearance: none;
-  &:focus {
-    outline: none;
-  }
-  &::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 1px;
-    cursor: pointer;
-    box-shadow: none;
-    background: #3fa9e4;
-    border-radius: 0px;
-    border: 0px solid #010101;
-  }
-  &::-moz-range-track {
-    width: 100%;
-    height: 1px;
-    cursor: pointer;
-    box-shadow: none;
-    background: #3fa9e4;
-    border-radius: 0px;
-    border: 0px solid #010101;
-  }
-
-  &::-webkit-slider-thumb {
-    box-shadow: none;
-    border: 0px solid #ffffff;
-    box-shadow: 0px 10px 10px rgba(0,0,0,0.25);
-    height: 42px;
-    width: 22px;
-    border-radius: 22px;
-    background: #3fa9e4;
-    cursor: pointer;
-    -webkit-appearance: none;
-    margin-top: -20px;
-  }
-  &::-moz-range-thumb{
-    box-shadow: none;
-    border: 0px solid #3fa9e4;
-    box-shadow: 0px 10px 10px rgba(0,0,0,0.25);
-    height: 42px;
-    width: 22px;
-    border-radius: 22px;
-    background: #3fa9e4;
-    cursor: pointer;
-    appearance: none;
-    -webkit-appearance: none;
-    margin-top: -20px;
-  }
-  &::-moz-focus-outer {
-    border: 0;
-  }
-}
-.rs-label {
-  position: relative;
-  transform-origin: center center;
-  display: block;
-  width: 50px;
-  height: 50px;
-  background: transparent;
-  border-radius: 50%;
-  line-height: 50px;
-  text-align: center;
-  font-weight: bold;
-  padding-top: 5px;
-  box-sizing: border-box;
-  border: 2px solid #3fa9e4;
-  margin-top: 10px;
-  left: attr(value);
-  color: #3fa9e4;
-  font-style: normal;
-  font-weight: normal;
-  line-height: normal;
-  font-size: 20px;
-  &::after {
-    content: "KM";
-    display: block;
-    font-size: 15px;
-    letter-spacing: 0.07em;
-    margin-top: -2px;
-  } 
 }
 </style>
 
